8d04e6e560cb5f5fc031a0a75fa454115c0eba74
Jaret Wright <hkkA223>
2020-09-30 13:55:48 -0400

    Updated with ... SceneChanger

--- src/CreateHandSanitizerController.java
+++ src/CreateHandSanitizerController.java
@@ -1,10 +1,17 @@
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -49,6 +56,18 @@ public class CreateHandSanitizerController implements Initializable {
                 outputLabel.setText(e.getMessage());
             }
         }
+    }
+
+    @FXML
+    void changeToListView(ActionEvent event) throws IOException {
+//        Parent root = FXMLLoader.load(getClass()
+//                .getResource("listOfBottlesView.fxml"));
+//        Scene scene = new Scene(root);
+//        Stage stage = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+//        stage.setScene(scene);
+//        stage.setTitle("Hand Sanitizer GUI cool thing");
+//        stage.show();
+        SceneChanger.changeScene(event,"listOfBottlesView.fxml","List View");
     }
 
     /**
--- src/ListOfBottlesController.java
+++ src/ListOfBottlesController.java
@@ -1,2 +1,23 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ListOfBottlesController {
+    @FXML
+    void changeToCreateView(ActionEvent event) throws IOException {
+//        Parent root = FXMLLoader.load(getClass()
+//                .getResource("createHandSanitizerView.fxml"));
+//        Scene scene = new Scene(root);
+//        Stage stage = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+//        stage.setScene(scene);
+//        stage.setTitle("Hand Sanitizer GUI cool thing");
+//        stage.show();
+        SceneChanger.changeScene(event,"createHandSanitizerView.fxml",
+                "create hand Sanitizer bottle");
+    }
 }
